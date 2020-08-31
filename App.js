414f48aadfcc50d61967cfd69d8dad6bda2afe32
Huy Manh <huymanhtmhp@gmail.com>
2020-08-31 21:19:42 +0700

    Init project

---
+++ App.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react'
+import { View, Text } from 'react-native'
+
+class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {}
+  }
+
+  render() {
+    return (
+      <View>
+        <Text> App </Text>
+      </View>
+    )
+  }
+}
+
+export default App
